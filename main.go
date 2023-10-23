dfb6fe691cecea8f6fafb5074cf611c9599d9532
David Nguyen <ddnn2026@gmail.com>
2023-10-24 00:19:31 +0700

    create

--- main.go
+++ main.go
@@ -20,6 +20,12 @@ type TodoItem struct {
 	Created_at  *time.Time `json:"created_at"`
 	Updated_at  *time.Time `json:"updated_at"`
 }
+type TodoItemCreate struct {
+	Id          int    `json:"-" gorm:"id;"`
+	Title       string `json:"title" gorm:"title;"`
+	Description string `json:"description" gorm:"description;"`
+	Status      string `json:"status" gorm:"status;"`
+}
 
 func main() {
 	dsn := "root:mypass@tcp(127.0.0.1:3306)/mydb?charset=utf8mb4&parseTime=True&loc=Local"
@@ -49,10 +55,41 @@ func main() {
 	fmt.Println(string(jsonData))
 
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": item,
-		})
-	})
+
+	v1 := r.Group("/v1")
+	{
+		v1.GET("")
+		v1.POST("", CreateItem(db))
+		v1.GET("/:id")
+		v1.PATCH("/:id")
+		v1.DELETE("/:id")
+	}
+
+	r.GET("/ping", getPing)
 	r.Run(":8088")
+}
+
+func getPing(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "pong",
+	})
+}
+
+func CreateItem(db *gorm.DB) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		var data TodoItemCreate
+		if err := c.ShouldBindJSON(&data); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+		}
+
+		result := db.Table("todo_items").Create(&data)
+		fmt.Println(result)
+
+		c.JSON(http.StatusOK, gin.H{
+			"id":     data.Id,
+			"effect": result.RowsAffected,
+		})
+	}
 }
